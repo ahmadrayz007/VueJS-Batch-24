0a0884d658e01d54a86ded1919496dc1291a9545
ahmadrayz007 <ahmadrayz007@gmail.com>
2021-06-12 02:40:20 +0700

    menambahkan index.js Array,Function,dan Object

---
+++ Tugas-5-Javascript-Array-Function-Object/index.js
@@ -0,0 +1,15 @@
+// Soal 1
+
+var daftarHewan = ["2. Komodo", "5. Buaya", "3. Cicak", "4. Ular", "1. Tokek"];
+daftarHewan.sort();
+console.log(daftarHewan);
+
+// Soal 2
+
+
+var data = {name : "John" , age : 30 , address : "Jalan Pelesiran" , hobby : "Gaming" }
+ 
+var perkenalan = introduce(data)
+console.log(perkenalan) // Menampilkan "Nama saya John, umur saya 30 tahun, alamat saya di Jalan Pelesiran, dan saya punya hobby yaitu Gaming" 
+
+// Soal 3
