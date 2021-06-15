4316071cb5c14d9fdcd025e0af02165a1ea3b1f4
ahmadrayz007 <ahmadrayz007@gmail.com>
2021-06-15 23:13:07 +0700

    menambahkan Tugas 7

---
+++ Tugas-7-Asynchronous/index2.js
@@ -0,0 +1,9 @@
+var readBooksPromise = require('./promise.js')
+ 
+var books = [
+    {name: 'LOTR', timeSpent: 3000}, 
+    {name: 'Fidas', timeSpent: 2000}, 
+    {name: 'Kalkulus', timeSpent: 4000}
+]
+
+readBooksPromise(10000,books[0,1,2]);
